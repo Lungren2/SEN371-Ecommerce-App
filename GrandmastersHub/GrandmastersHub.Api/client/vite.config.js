12bd5031af464a3a717cccd86c82ac95e2975b0d
Kimberly90 <kimberlykaronga2@gmail.com>
2026-09-16 17:48:02 +0200

    Milestone 5 Person 2 - Frontend deployment

--- GrandmastersHub/GrandmastersHub.Api/client/vite.config.js
+++ GrandmastersHub/GrandmastersHub.Api/client/vite.config.js
@@ -3,13 +3,14 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5188',
-        changeOrigin: true,
-      },
+    base: '/SEN371-Ecommerce-App/',
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5188',
+                changeOrigin: true,
+            },
+        },
     },
-  },
 })
